a3711164a30ba35fa4c15227bb45488127387172
Troy Crawford <tacrawford91@gmail.com>
2018-03-12 19:31:22 -0500

    Application Complete

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    database: 'wings_db',
-    user: 'root',
-    password: 'Makerasll91!',
-    port: 3306
-});
+var connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: '127.0.0.1',
+        database: 'wings_db',
+        user: 'root',
+        password: 'Makerasll91!',
+        port: 3306
+    });
+}
+connection.connect();
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 // process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
